47f1af953a01cd04f59ed1826d321ce14f2d7aa3
Sergei Kossik <fretfan92@gmail.com>
2020-03-21 11:24:21 +0200

    add core module

---
+++ core/src/main/java/com/seroga/core/MyStringUtils.java
@@ -0,0 +1,9 @@
+package com.seroga.core;
+
+
+public class MyStringUtils {
+
+    public static String addPrefix(String input) {
+        return "SomePrefix__" + input;
+    }
+}
--- src/main/java/com/seroga/bootgradle/SomeApi.java
+++ src/main/java/com/seroga/bootgradle/SomeApi.java
@@ -1,4 +1,5 @@
 package com.seroga.bootgradle;
+import com.seroga.core.MyStringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,8 @@ public class SomeApi {
     @GetMapping("/1")
     public String hello() {
         String message =  "Hello 12312412!";
-        String msg2 = message;
-//        String msg2 = MyStringUtils.addPrefix(message);
+//        String msg2 = message;
+        String msg2 = MyStringUtils.addPrefix(message);
         return msg2;
     }
 }
